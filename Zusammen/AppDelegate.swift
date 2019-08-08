d980c89e0c930e8f4466f9d5eaaa9a736487c86a
Karthikeya Udupa <karthikeyaudupa@gmail.com>
2019-08-08 12:02:33 +0200

    Initial Commit

---
+++ Zusammen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Zusammen
+//
+//  Created by Karthikeya Udupa on 08/08/2019.
+//  Copyright © 2019 Karthikeya Udupa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Zusammen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zusammen
+//
+//  Created by Karthikeya Udupa on 08/08/2019.
+//  Copyright © 2019 Karthikeya Udupa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
